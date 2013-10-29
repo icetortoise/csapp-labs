40cb70fe5c848ee3fd26c564c7cb50fcd3e58b6a
Andy Wu <andywu@mint>
2013-10-29 16:56:06 -0400

    finally got csim right..

--- cachelab-handout/csim.c
+++ cachelab-handout/csim.c
@@ -1,6 +1,8 @@
+#define _GNU_SOURCE
 #include "cachelab.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <getopt.h>
 
 
 typedef unsigned long long Address;
@@ -8,6 +10,7 @@ typedef unsigned long long Address;
 typedef struct CacheLine{
     short valid;
     Address tag;
+    unsigned long long lu;
 } CacheLine;
 
 typedef struct CacheSet{
@@ -15,13 +18,19 @@ typedef struct CacheSet{
     CacheLine *line;
 } CacheSet;
 
+CacheSet *cache;
+int verbose = 1;
+unsigned long long glu = 0;
+int s, e, b;
+
 CacheSet *initCache(int s, int e){
     int nset = 2 << (s-1);
-    CacheSet *ptr = (CacheSet *)malloc(nset*sizeof(CacheSet));
-    CacheLine *line_ptr = (CacheLine *)realloc(ptr, nset*sizeof(CacheSet) + e*sizeof(CacheLine));
+    CacheSet *ptr = (CacheSet *)malloc((nset*sizeof(CacheSet)) + (nset*e*sizeof(CacheLine)));
+    CacheLine *line_ptr = (CacheLine *)(ptr + nset);
     for(int i = 0; i < nset; i++){
 	(ptr+i)->length = e;
-	(ptr+i)->line = line_ptr++;
+	(ptr+i)->line = line_ptr;
+	line_ptr += e;
 	((ptr+i)->line)->valid = 0;
 	((ptr+i)->line)->tag = 0;
     }
@@ -31,34 +40,146 @@ CacheSet *initCache(int s, int e){
 // util functions to process the addresses
 
 int addr2setidx(Address addr, int s, int b){
-    addr >>= b;
-    int tagbits = sizeof(addr) - s - b;
+    int tagbits = 8*sizeof(addr) - s - b;
     addr = (addr << tagbits) >> tagbits;
-    return (int)addr;
+    addr >>= b;
+    return ((int)addr);
 }
 
 Address addr2tag(Address addr, int s, int b){
     return addr >> (b+s);
 }
 
-void processLine(int *miss, int *hit, int *envic, char *line, int verbose){
-    
+void handleLoad(int *miss, int *hit, int *envic, char *line){
+    Address addr;
+    unsigned int a;
+    int length;
+    sscanf(line, "%x,%d", &a, &length);
+    addr = (Address) a;
+    int setidx = addr2setidx(addr, s, b);
+    printf("setidx is %d\n", setidx);
+    Address tag = addr2tag(addr, s, b);
+    CacheLine *cline = (cache + setidx) -> line;
+    int lineidx = -1;
+    int lru = -1;
+    unsigned long long lu = 0xffffffff;
+    for(int i = 0; i < (cache + setidx) -> length; i++){
+	if(cline[i].valid == 0 && lineidx < 0){
+	    lineidx = i;
+	}else if(cline[i].valid > 0){
+	    if(cline[i].lu < lu){
+		lu = cline[i].lu;
+		lru = i;
+	    }
+	    if(cline[i].tag == tag){
+		if(verbose){
+		    printf("%s\n", "Load hit!");
+		}
+		*hit += 1;
+		cline[i].lu = ++glu;
+		return;
+	    }
+	}
+    }
+    if(verbose){
+	printf("%s\n", "Load miss!");
+    }
+    *miss += 1;
+    if(lineidx < 0) {
+	lineidx = lru;
+	if(verbose)
+	    printf("%s\n", "Enviction!");
+	*envic += 1;
+	/* printf("lineidx: %d\n", lineidx); */
+	/* printf("envic: %d\n", *envic); */
+    }
+    cline[lineidx].valid = 1;
+    cline[lineidx].tag = tag;
+    cline[lineidx].lu = ++glu;
+}
+/* void handleStore(int *miss, int *hit, int *envic, char *line){} */
+/* void handleModify(int *miss, int *hit, int *envic, char *line){} */
+
+void processLine(int *miss, int *hit, int *envic, char *line){
+    if(line[0] != ' '){
+	return;
+    }else{
+	if(line[1] == '\0' || line[2] == '\0'){
+	    return;
+	}
+	switch(line[1]){
+	case 'L':
+	    handleLoad(miss, hit, envic, line + 3);
+	    break;
+	case 'S':
+	    handleLoad(miss, hit, envic, line + 3);
+	    break;
+	case 'M':
+	    handleLoad(miss, hit, envic, line + 3);
+	    handleLoad(miss, hit, envic, line + 3);
+	    break;
+	default:
+	    return;
+	}
+    }
 }
 
-int main()
+
+int main(int argc, char **argv)
 {
-    // init cache
-    CacheSet *cache = initCache(4, 1);
-    
-    /* for(int i = 0; i < (2<<3); i++){ */
-    /* 	if(cache[i].line->valid != 0){ */
-    /* 	    printf("crap!"); */
-    /* 	} */
-    /* } */
-    // printf("%lx ", (long unsigned int)addr2tag(0xefef1234, 15, 8));
-    
+    verbose = 0;
+    // opt processing
+    char *filename;
+    int c;
+    short result = 0;
+    while((c = getopt(argc, argv, "vs:E:b:t:")) != -1){
+	switch(c){
+	case 'v':
+	    verbose = 1;
+	    break;
+	case 's':
+	    s = atoi(optarg);
+	    result |= 0x01;
+	    break;
+	case 'E':
+	    e = atoi(optarg);
+	    result |= 0x02;
+	    break;
+	case 'b':
+	    b = atoi(optarg);
+	    result |= 0x04;
+	    break;
+	case 't':
+	    filename = optarg;
+	    result |= 0x08;
+	    break;
+	case '?':
+	    printf("%s\n", "error in the args!");
+	    return 1;
+	}
+    } 
+    if(result != 0x0f){
+	printf("%s \n", "Missing args!");
+	return 1;
+    }
+
+    cache = initCache(s, e);
     // read valgrind file
+    FILE *fp = fopen(filename, "r");
+    char *l; size_t len = 0;
+    if(fp == NULL){
+	printf("%s\n", "file does not exist!");
+	return 1;
+    }
+    int miss = 0, hit = 0, envic = 0;
+    while(getline(&l, &len, fp) != -1){
+	if(verbose)
+	    printf("%s", l);
+	processLine(&miss, &hit, &envic, l);
+    }
+
+    free(l);
     free(cache);
-    printSummary(0, 0, 0);
+    printSummary(hit, miss, envic);
     return 0;
 }
